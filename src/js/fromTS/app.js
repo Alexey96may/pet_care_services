aa00aff0fac74e0bb3fbf2bcac8e59c5bf259b80
Alexey96may <shulga_alexey@vk.com>
2025-09-09 11:08:16 +0300

    Add Burger Menu and Finish the header

--- src/js/fromTS/app.js
+++ src/js/fromTS/app.js
@@ -1,7 +1,7 @@
 import * as flsFunctions from "./modules/functions.js";
 import { Burger } from "./ui/burger.js";
 function my_test(x) {
-    var answer = x > 2 ? true : false;
+    let answer = x > 2 ? true : false;
     return answer;
 }
 my_test(24);
--- src/js/fromTS/ui/burger.js
+++ src/js/fromTS/ui/burger.js
@@ -1,55 +1,16 @@
-var Burger = /** @class */ (function () {
-    function Burger() {
-        var _this = this;
-        var _a, _b, _c;
-        this.BURGER_BUTTON = document.getElementById("burger");
-        this.CANCEL_BUTON = document.getElementById("cancel");
-        this.ACTIVE_FIELD = document.getElementById("activeField");
-        this.HEADER = document.getElementById("header");
-        this.BODY = document.querySelector("body");
-        this.HEADER_NAV = document.getElementById("headerNav");
-        this.isMenuOpen = false;
-        (_a = this.BURGER_BUTTON) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function (event) {
+export class Burger {
+    #burgerButton = document.getElementById("burger");
+    #headerNav = document.getElementById("headerNav");
+    #isMenuOpen = false;
+    constructor() {
+        this.#burgerButton?.addEventListener("click", (event) => {
             event.stopPropagation();
-            _this.isMenuOpen = true;
-            _this.changeOverflow(_this.BODY, "hidden");
-            _this.bBH_classesToggle();
-        });
-        (_b = this.CANCEL_BUTON) === null || _b === void 0 ? void 0 : _b.addEventListener("click", function (event) {
-            event.stopPropagation();
-            _this.isMenuOpen = false;
-            _this.changeOverflow(_this.BODY, "auto");
-            _this.bBH_classesToggle();
-        });
-        (_c = this.BODY) === null || _c === void 0 ? void 0 : _c.addEventListener("click", function (event) {
-            var _a;
-            if (event.target.closest("#headerNav")) {
-                if (_this.isMenuOpen && ((_a = event.target.tagName) === null || _a === void 0 ? void 0 : _a.toLowerCase()) === "a") {
-                    _this.isMenuOpen = false;
-                    _this.changeOverflow(_this.BODY, "auto");
-                    _this.bBH_classesToggle();
-                }
-                return;
-            }
-            if (_this.isMenuOpen) {
-                _this.isMenuOpen = false;
-                _this.changeOverflow(_this.BODY, "auto");
-                _this.bBH_classesToggle();
-            }
+            this.#isMenuOpen = true;
+            this.classesToggle();
         });
     }
-    Burger.prototype.bBH_classesToggle = function () {
-        var _a, _b, _c, _d;
-        (_a = this.ACTIVE_FIELD) === null || _a === void 0 ? void 0 : _a.classList.toggle("inactive_field");
-        (_b = this.HEADER) === null || _b === void 0 ? void 0 : _b.classList.toggle("inactive_header");
-        (_c = this.BURGER_BUTTON) === null || _c === void 0 ? void 0 : _c.classList.toggle("inactive_button");
-        (_d = this.HEADER_NAV) === null || _d === void 0 ? void 0 : _d.classList.toggle("appear");
-    };
-    Burger.prototype.changeOverflow = function (element, overflowStyle) {
-        if (element) {
-            element.style.overflow = overflowStyle;
-        }
-    };
-    return Burger;
-}());
-export { Burger };
+    classesToggle() {
+        this.#burgerButton?.classList.toggle("burger--cancel");
+        this.#headerNav?.classList.toggle("appear");
+    }
+}
