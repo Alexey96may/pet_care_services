498e84d2e1bfe3ea3855aeed0433db680f6d0219
Alexey96may <shulga_alexey@vk.com>
2025-09-09 11:10:38 +0300

    commit temporal files

--- src/js/fromTS/app.js
+++ src/js/fromTS/app.js
@@ -1,6 +1,6 @@
 import * as flsFunctions from "./modules/functions.js";
 function my_test(x) {
-    var answer = x > 2 ? true : false;
+    let answer = x > 2 ? true : false;
     return answer;
 }
 my_test(24);
